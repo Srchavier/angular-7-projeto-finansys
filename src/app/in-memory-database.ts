bc817825fa1f549becb8c10cbc16f39e7c2b88a6
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2019-01-20 20:28:42 -0200

    Adicionando categoria model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,14 +1,16 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model';
+
 export class InMemoryDataBase implements InMemoryDbService {
 
     createDb() {
-        const categories = [
-            {id: 1, name: 'Lazer', description: 'Pagamentos de Contas'},
-            {id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios'},
-            {id: 3, name: 'Moradia', description: 'Cimena, parques, festa etc.'},
-            {id: 4, name: 'Salário', description: 'Recebimento do salário'},
-            {id: 5, name: 'Freelas', description: 'Trabalhando como freelancer'},
+        const categories: Category[] = [
+            { id: 1, name: 'Lazer', description: 'Pagamentos de Contas' },
+            { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
+            { id: 3, name: 'Moradia', description: 'Cimena, parques, festa etc.' },
+            { id: 4, name: 'Salário', description: 'Recebimento do salário' },
+            { id: 5, name: 'Freelas', description: 'Trabalhando como freelancer' },
         ];
 
         return { categories };
