1017d35494070f5fd8766d6e29e86fc78a77fc1b
eduardo <rodrigues>
2019-02-09 20:18:14 -0200

    refatoração do projeto

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,11 @@
-export class Category {
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
+
+export class Category extends BaseResourceModel {
     constructor(
         public id?: Number,
         public name?: String,
         public description?: String
-    ) { }
+    ) {
+        super();
+    }
 }
